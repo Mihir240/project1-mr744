6f5510efc77edf2686dc217b88bd0919dc430c38
Mihir240 <mihirr240@gmail.com>
2021-02-06 23:24:32 +0000

    Updated the artist name positioning and organized code in main.py and track.py

--- main.py
+++ main.py
@@ -7,60 +7,36 @@ from track import *
 
 app = Flask(__name__)
 
-def extract_track_info(json_obj: dict) -> list:
-    all_tracks = []
-    
-    #traverse the json_obj
-    for info in json_obj['tracks'][:10]:
-
-        song_artist = info['artists'][0]['name']
-        song_name = info['name']
-        song_image = info['album']['images'][1]['url']
-        song_url = info['preview_url']
-        
-        
-        all_tracks.append(
-            Track(song_name,
-                song_artist,
-                song_image,
-                song_url,
-                )
-            )
-            
-    return random.sample(all_tracks,3)
 
 @app.route('/')
 def main_app():
     
-    #spotify object handle
-    sp = spot_obj()
+    #spotify object that handles the get requests
+    spotify_obj = spot_obj()
     
-                  #drake, weeknd, post malone, adele, lorde
-    id_artists = ['3TVXtAsR1Inumwj472S9r4','1Xyo4u8uXC1ZmMpatF05PJ','246dkjvS1zLTtiykXe5h60','4dpARuHxo51G3z768sgnrY','163tK9Wjr9P9DmM0AVK7lm']
+    #id's for fav artists
+    id_artists = ['3TVXtAsR1Inumwj472S9r4', #drake
+                '1Xyo4u8uXC1ZmMpatF05PJ', #weeknd
+                '246dkjvS1zLTtiykXe5h60', #post malone
+                '4dpARuHxo51G3z768sgnrY', #adele
+                '163tK9Wjr9P9DmM0AVK7lm', #lorde
+                '06HL4z0CvFAxyc27GXpf02', #taylor swift
+                '6eUKZXaKkcviH0Ku9w2n3V'] #ed sheeran
     
-    #choose one id artist from the array
+    #chooses one id artist from the list
     selected_id = random.choice(id_artists)
-    
-    #pass json format in the track obj
-    json_obj = sp.artist_top_tracks(selected_id,country='US')
-    
-    #pass it into 
+    json_obj = spotify_obj.artist_top_tracks(selected_id,country='US')
     list_of_tracks = extract_track_info(json_obj)
     
-    
-    artist = sp.artist(selected_id)
-    #artist image url
-    image_of_artist = artist['images'][2]['url']
-    
-    #artist name
-    artist_name = artist['name']
-    
-    print(image_of_artist)
+    #retrieving artist info
+    artist = spotify_obj.artist(selected_id)
+                        # artist pic                 #artist name
+    info_of_artist = [artist['images'][2]['url'], artist['name']  ]
+
     return render_template(
         'index.html',
         all_tracks = list_of_tracks,
-        url_artist = image_of_artist,
-        artist_name = artist_name,
+        artist_info = info_of_artist,
     )
 
 app.run(
